5dd08fe2e645ceeb623e455f368a96b485b3bc47
Samuel May <sam.m4y@gmail.com>
2009-07-04 23:08:37 +1000

    Added complexity.clj interface script. Also improved the way external programs are run.

---
+++ complexity.clj
@@ -0,0 +1,42 @@
+(use '(functerpolate regression 
+		     complexity)
+     '(clojure.contrib command-line))
+
+(defn parse-number-list [lst]
+  (map #(Double/parseDouble %) 
+       (re-seq #"[-+]?[0-9]+(?:\.[0-9]+)?" lst)))
+
+(defn parseint [arg]
+  (cond 
+    (number? arg) arg
+    (string? arg) (Integer/parseInt arg)
+    :else nil))
+
+(with-command-line *command-line-args*
+  "complexity.clj: attempt to detirmine the time complexity vs input size for a 
+given program.
+
+The required arguments are:
+ - an executable program to test
+ - a second program that, given a number as an argument, will generate 
+   appropriate input of that size for the first program.
+"
+  [[start s "starting input size" 100]
+   [limit l "final input size" 1000]
+   [points p "number of data points to collect" 10]
+   [runs r "how many times to run the program for each data point" 10]
+   [plot? p? "if given, create a graphical plot of the results" false]
+   [extrapolate e "a list of bigger input sizes to extrapolate for"]
+   rest-of-args]
+  (let [[program input-generator & dont-care] rest-of-args
+	numerical-opts (interleave [:start :limit :npoints :runs]
+				   (map parseint 
+					(list start limit points runs)))]
+    (apply complexity
+	   program 
+	   input-generator 
+	   :plot plot?
+	   ;; nil counts as an empty collection, which will be handled properly
+	   ;; by the function
+	   :extrapolate extrapolate
+	   numerical-opts)))
--- functerpolate/complexity.clj
+++ functerpolate/complexity.clj
@@ -5,16 +5,33 @@
   (:refer-clojure :exclude (time))
   (:use (clojure.contrib [def :only (defmacro- defnk)]
 			 shell-out)
-	(functerpolate regression plot)))
+	(functerpolate regression plot))
+  (:import javax.swing.JFrame))
+
+;; Yay OS dependance. I used to do the executing and piping in pure java, using
+;; Runtime.exec and streams, but the overhead was ridiculous.
+(defmacro os-program-runner [executable command-format]
+  `(fn [program# input-generator# input-size#]
+     (sh ~@executable 
+	 (format ~command-format 
+		 program# input-generator# (str input-size#)))))
+
+(def run-program 
+     (let [name (System/getProperty "os.name")]
+       (cond
+	 (some #(= name %) (list "Linux" "Mac OS X"))
+	 (os-program-runner ("/bin/sh" "-c") "%2$s %3$s | %1$s > /dev/null")
+	 ;; THIS HAS NOT ACTUALLY BEEN TESTED ON WINDOWS
+	 (some #(= name %) (list "Windows XP" "Windows Vista"))
+	 (os-program-runner ("cmd.exe" "\\C") "%2$s %3$s | %1$s > NUL"))))
 
 (def *runs* 10)
 
 (defmacro- time [expr]
-  `(let [start-time#  (System/currentTimeMillis)
-	 result#      ~expr ;; we time this bit!
-	 finish-time# (System/currentTimeMillis)]
+  `(let [start#  (System/nanoTime)
+	 result#      ~expr] ;; we time this bit
      ;; return time in seconds
-     (float (/ (- finish-time# start-time#) 1000))))
+     (/ (double (- (System/nanoTime) start#)) 1000000000.0)))
 
 (defmacro- average-multiple-runs [form]
   `(let [results# (for [i# (range *runs*)]
@@ -36,7 +53,7 @@
 (defmethod run-time :external-program
   [program input-generator input-size]
   (average-multiple-runs
-   (sh program :in (sh input-generator (str input-size)))))
+   (run-program program input-generator input-size)))
 
 (def *big-O-map*
      {:Linear		"O(N), or linear-time",
@@ -61,8 +78,11 @@
 	  regression (fit-model :Best n runtimes)
 	  predicted  (map (:function regression) extrapolate)
 	  bigO       ((:model regression) *big-O-map*)]
+      ;; more needs to be done here
       (when plot
-	(plot-fitted-curve regression "Plot of Time Complexity" n runtimes))
+	(. (:frame (plot-fitted-curve regression "Plot of Time Complexity" 
+				      n runtimes)) 
+	   setDefaultCloseOperation JFrame/EXIT_ON_CLOSE))
       (when print
 	(println (format "The suggested complexity is %s." bigO))
 	(println (format "Input size    Average runtime over %d runs" *runs*))
