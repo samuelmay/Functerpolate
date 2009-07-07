eeb9927145a966085cb8ef2c8fb2582921c4a553
Samuel May <sam.m4y@gmail.com>
2009-07-07 15:50:14 +1000

    Update and add docstrings.

--- functerpolate/interpolate.clj
+++ functerpolate/interpolate.clj
@@ -1,12 +1,15 @@
 (ns 
     #^{:author "Samuel May"
-       :doc "Functional interpolation and linear regression tools"}
+       :doc "Functions for interpolation of data"}
   functerpolate.interpolate 
   (:use [clojure.contrib.seq-utils :only (rotations)]))
 
 ;; References:
 ;; R. Hornbeck, "Numerical Methods", pg. 43
 (defn lagrange-interpolation-fn [xseries yseries]
+  "Given a sequence of n x-values and n corresponding-y values, returns a 
+clojure function implementing a nth-order polynomial, that passes through all 
+the given points. The method of Lagrange polynomials is used."
   (let [Pi (map (fn [[[xj fj] & other-points]]
 		  (let [xi (map first other-points)]
 		    (fn [x]
